1b5822b41db35dc93136746101e8c555755bf807
Sanketh <sankethsequeira@gmail.com>
2023-04-11 01:25:06 +0530

    Window resizable

--- Tic tac toe GUI.py
+++ Tic tac toe GUI.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 #from tkinter import Button
 
+
 #create the main window
 root=Tk()
 root.title("Tic-Tac-Toe")
@@ -68,37 +69,53 @@ def check_win():
         disable_all_buttons()
         messagebox.showinfo("Game ends without a winner","It is a draw!")
 
+#creating frame
+frame= Frame(root, relief= 'sunken')
+frame.pack(fill= BOTH, expand= True, padx= 30, pady=20)
+
 #create buttons
 
-b1=Button(root, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b1))
-b2=Button(root, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b2))
-b3=Button(root, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b3))
+b1=Button(frame, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b1))
+b2=Button(frame, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b2))
+b3=Button(frame, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b3))
 
-b4=Button(root, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b4))
-b5=Button(root, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b5))
-b6=Button(root, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b6))
+b4=Button(frame, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b4))
+b5=Button(frame, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b5))
+b6=Button(frame, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b6))
 
-b7=Button(root, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b7))
-b8=Button(root, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b8))
-b9=Button(root, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b9))
+b7=Button(frame, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b7))
+b8=Button(frame, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b8))
+b9=Button(frame, text="", font=("Times New Roman",20), height=5, width=10, bg="SystemButtonFace", command=lambda: b_click(b9))
 
 #create restart button
 brestart=Button(root, text="Restart Game", font=("Times New Roman",15), height=2, width=25, bg="SystemButtonFace", command=lambda: restart())
 
 #create buttons grid
 
-b1.grid(row=0, column=0)
-b2.grid(row=0, column=1)
-b3.grid(row=0, column=2)
+b1.grid(row=0, column=0, sticky="NSEW")
+b2.grid(row=0, column=1, sticky="NSEW")
+b3.grid(row=0, column=2, sticky="NSEW")
 
-b4.grid(row=1, column=0)
-b5.grid(row=1, column=1)
-b6.grid(row=1, column=2)
+b4.grid(row=1, column=0, sticky="NSEW")
+b5.grid(row=1, column=1, sticky="NSEW")
+b6.grid(row=1, column=2, sticky="NSEW")
 
-b7.grid(row=2, column=0)
-b8.grid(row=2, column=1)
-b9.grid(row=2, column=2)
+b7.grid(row=2, column=0, sticky="NSEW")
+b8.grid(row=2, column=1, sticky="NSEW")
+b9.grid(row=2, column=2, sticky="NSEW")
+
+#resize columns
+frame.columnconfigure(0, weight=1)
+frame.columnconfigure(1, weight=1)
+frame.columnconfigure(2, weight=1)
+
+#resize rows
+frame.rowconfigure(0, weight=1)
+frame.rowconfigure(1, weight=1)
+frame.rowconfigure(2, weight=1)
 
 #place restart button
-brestart.place(x=100, y=550)
+brestart.pack()
+
+
 root.mainloop()
